47c303db98d466c102e10561c492edf85939ccb7
prasad chauhan <67370566+Prasadchauhan99@users.noreply.github.com>
2024-11-18 16:33:38 +0530

    Add files via upload

---
+++ FileJava.java
@@ -0,0 +1,34 @@
+package com.in.out;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileHandling {
+
+	public static void main(String args[])throws IOException
+	{
+		
+		//File f = new File("C:\\prasad\\FilesHaldling\\ChauhanFiles.txt");
+		File f1=new File("C:\\Users\\prasa\\eclipse-workspace\\FileHandlingInJava");
+		
+		for(String fileName:f1.list())
+		{
+			System.out.println(fileName);
+		}
+		
+		//System.out.println(f1.mkdirs());
+		
+		//System.out.println(f.createNewFile());
+		//System.out.println(f1.createNewFile());
+		
+		//System.out.println(f.delete());
+		//System.out.println(f1.delete());
+		
+		
+	//System.out.println(f1.canWrite());
+	//f1.setWritable(true);
+	//System.out.println(f1.canWrite());
+		
+	}
+	
+}
